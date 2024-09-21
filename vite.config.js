d450b8bc05a09aff915bf3f9406bbfb621fd5ad2
Mohamed Bankolé <medbankole97@gmail.com>
2024-09-21 11:43:28 +0000

    configurer les alias dans vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';  // Ajoute 'path' pour gérer les chemins
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),  // Ajout de l'alias '@' pour le dossier 'src'
+    },
+  },
+});
